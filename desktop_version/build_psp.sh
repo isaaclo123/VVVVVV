cd7b2c1b9d204f4d7484ddbb009476dc39445906
Isaac Lo <isaaclo123@gmail.com>
2021-01-10 21:58:05 -0800

    build script

--- desktop_version/build_psp.sh
+++ desktop_version/build_psp.sh
@@ -6,6 +6,7 @@ make clean
 
 cmake . \
     -DCMAKE_VERBOSE_MAKEFILE=1 \
+    -DCMAKE_TOOLCHAIN_FILE="$PSPDEV/psp/share/cmake/PSP.cmake" \
     -DSDL2_INCLUDE_DIRS="$PSPDEV/psp/include/SDL2" \
     -DSDL2_LIBRARIES="$PSPDEV/psp/lib/libSDL.a;$PSPDEV/psp/lib/libSDL2main.a;$PSPDEV/psp/lib/libSDL2_mixer.a" &&
 
