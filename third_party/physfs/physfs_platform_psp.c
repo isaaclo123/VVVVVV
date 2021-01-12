1f5e900161a8c6c631becbd0f158f001ea2ba5de
Isaac Lo <isaaclo123@gmail.com>
2021-01-12 15:06:24 -0800

    replace io methods with psp sdk methods in physfs

--- third_party/physfs/physfs_platform_psp.c
+++ third_party/physfs/physfs_platform_psp.c
@@ -23,6 +23,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <time.h>
 
 #include "physfs_internal.h"
 
@@ -63,16 +64,16 @@ PHYSFS_EnumerateCallbackResult __PHYSFS_platformEnumerate(const char *dirname,
                                PHYSFS_EnumerateCallback callback,
                                const char *origdir, void *callbackdata)
 {
-    DIR *dir;
-    struct dirent *ent;
+    SceUID dir;
+    SceIoDirent ent;
     PHYSFS_EnumerateCallbackResult retval = PHYSFS_ENUM_OK;
 
-    dir = opendir(dirname);
-    BAIL_IF(dir == NULL, errcodeFromErrno(), PHYSFS_ENUM_ERROR);
+    dir = sceIoDopen(dirname);
+    BAIL_IF(dir > 0, errcodeFromErrno(), PHYSFS_ENUM_ERROR);
 
-    while ((retval == PHYSFS_ENUM_OK) && ((ent = readdir(dir)) != NULL))
+    while ((retval == PHYSFS_ENUM_OK) && (sceIoDread(dir, &ent) > 0))
     {
-        const char *name = ent->d_name;
+        const char *name = ent.d_name;
         if (name[0] == '.')  /* ignore "." and ".." */
         {
             if ((name[1] == '\0') || ((name[1] == '.') && (name[2] == '\0')))
@@ -84,32 +85,46 @@ PHYSFS_EnumerateCallbackResult __PHYSFS_platformEnumerate(const char *dirname,
             PHYSFS_setErrorCode(PHYSFS_ERR_APP_CALLBACK);
     } /* while */
 
-    closedir(dir);
+    sceIoDclose(dir);
 
     return retval;
 } /* __PHYSFS_platformEnumerate */
 
+static inline PHYSFS_sint64 getEpochTime(ScePspDateTime* dt, struct tm* t) {
+    t->tm_year = dt->year;
+    t->tm_mon = dt->month;           // Month, where 0 = jan
+    t->tm_mday = dt->day;          // Day of the month
+    t->tm_hour = dt->hour;
+    t->tm_min = dt->minute;
+    t->tm_sec = dt->microsecond;
+    t->tm_isdst = 1;        // Is DST on? 1 = yes, 0 = no, -1 = unknown
+
+    return (PHYSFS_sint64) mktime(t);
+}
+
 int __PHYSFS_platformStat(const char *fname, PHYSFS_Stat *st, const int follow)
 {
     // TODO
-    struct stat statbuf;
+    SceIoStat statbuf;
+    struct tm t;
+
     // const int rc = follow ? stat(fname, &statbuf) : lstat(fname, &statbuf);
-    const int rc = stat(fname, &statbuf);
+    const int rc = sceIoGetstat(fname, &statbuf);
     BAIL_IF(rc == -1, errcodeFromErrno(), 0);
 
-    if (S_ISREG(statbuf.st_mode))
+    if (FIO_S_ISREG(statbuf.st_mode))
     {
         st->filetype = PHYSFS_FILETYPE_REGULAR;
         st->filesize = statbuf.st_size;
     } /* if */
 
-    else if(S_ISDIR(statbuf.st_mode))
+    else if(FIO_S_ISDIR(statbuf.st_mode))
     {
         st->filetype = PHYSFS_FILETYPE_DIRECTORY;
         st->filesize = 0;
     } /* else if */
 
-    else if(S_ISLNK(statbuf.st_mode))
+    else if(FIO_S_ISDIR(statbuf.st_mode))
     {
         st->filetype = PHYSFS_FILETYPE_SYMLINK;
         st->filesize = 0;
@@ -121,11 +136,11 @@ int __PHYSFS_platformStat(const char *fname, PHYSFS_Stat *st, const int follow)
         st->filesize = statbuf.st_size;
     } /* else */
 
-    st->modtime = statbuf.st_mtime;
-    st->createtime = statbuf.st_ctime;
-    st->accesstime = statbuf.st_atime;
+    st->modtime = getEpochTime(&(statbuf.st_mtime), &t);
+    st->createtime = getEpochTime(&(statbuf.st_ctime), &t);
+    st->accesstime = getEpochTime(&(statbuf.st_atime), &t);
 
-    st->readonly = (access(fname, W_OK) == -1);
+    st->readonly = ((statbuf.st_mode & PSP_O_RDONLY) == PSP_O_RDONLY);
     return 1;
 } /* __PHYSFS_platformStat */
 
@@ -336,21 +351,21 @@ int __PHYSFS_platformMkDir(const char *path)
 static void *doOpen(const char *filename, int mode)
 {
     const int appending = (mode & PSP_O_APPEND);
-    int fd;
+    SceUID fd;
     int *retval;
     errno = 0;
 
     /* O_APPEND doesn't actually behave as we'd like. */
     mode &= ~PSP_O_APPEND;
 
-    fd = open(filename, mode, S_IRUSR | S_IWUSR);
+    fd = sceIoOpen(filename, mode, 0777);
     BAIL_IF(fd < 0, errcodeFromErrno(), NULL);
 
     if (appending)
     {
-        if (lseek(fd, 0, SEEK_END) < 0)
+        if (sceIoLseek(fd, 0, SEEK_END) < 0)
         {
-            close(fd);
+            sceIoClose(fd);
             BAIL(errcodeFromErrno(), NULL);
         } /* if */
     } /* if */
@@ -358,7 +373,7 @@ static void *doOpen(const char *filename, int mode)
     retval = (int *) allocator.Malloc(sizeof (int));
     if (retval == NULL)
     {
-        close(fd);
+        sceIoClose(fd);
         BAIL(PHYSFS_ERR_OUT_OF_MEMORY, NULL);
     } /* if */
 
@@ -394,7 +409,7 @@ PHYSFS_sint64 __PHYSFS_platformRead(void *opaque, void *buffer,
     if (!__PHYSFS_ui64FitsAddressSpace(len))
         BAIL(PHYSFS_ERR_INVALID_ARGUMENT, -1);
 
-    rc = read(fd, buffer, (size_t) len);
+    rc = sceIoRead(fd, buffer, (size_t) len);
     BAIL_IF(rc == -1, errcodeFromErrno(), -1);
     assert(rc >= 0);
     assert(rc <= len);
@@ -411,7 +426,8 @@ PHYSFS_sint64 __PHYSFS_platformWrite(void *opaque, const void *buffer,
     if (!__PHYSFS_ui64FitsAddressSpace(len))
         BAIL(PHYSFS_ERR_INVALID_ARGUMENT, -1);
 
-    rc = write(fd, (void *) buffer, (size_t) len);
+    rc = sceIoWrite(fd, (void *) buffer, (size_t) len);
+
     BAIL_IF(rc == -1, errcodeFromErrno(), rc);
     assert(rc >= 0);
     assert(rc <= len);
@@ -430,7 +446,7 @@ int __PHYSFS_platformSeek(void *opaque, PHYSFS_uint64 pos)
       int rc = llseek(fd, offset_high, offset_low, &retoffset, SEEK_SET);
       BAIL_IF(rc == -1, errcodeFromErrno(), 0);
     #else
-      BAIL_IF(lseek(fd, (int) pos, SEEK_SET) == -1, errcodeFromErrno(), 0);
+      BAIL_IF(sceIoLseek(fd, (SceOff) pos, SEEK_SET) == -1, errcodeFromErrno(), 0);
     #endif
 
     return(1);
@@ -448,7 +464,8 @@ PHYSFS_sint64 __PHYSFS_platformTell(void *opaque)
       BAIL_IF(rc == -1, errcodeFromErrno(), -1);
       retval = (PHYSFS_sint64) retoffset;
     #else
-      retval = (PHYSFS_sint64) lseek(fd, 0, SEEK_CUR);
+      retval = (PHYSFS_sint64) sceIoLseek(fd, 0, SEEK_CUR);
+
       BAIL_IF(retval == -1, errcodeFromErrno(), -1);
     #endif
 
@@ -460,10 +477,10 @@ PHYSFS_sint64 __PHYSFS_platformFileLength(void *opaque)
 {
     int fd = *((int *) opaque);
     off_t len = -1;
-    off_t cur = lseek(fd, 0, SEEK_CUR);
+    off_t cur = sceIoLseek(fd, 0, SEEK_CUR);
 
-    BAIL_IF((len=lseek(fd, 0, SEEK_END)) == -1, errcodeFromErrno(), -1);
-    lseek(fd, cur, SEEK_SET);
+    BAIL_IF((len=sceIoLseek(fd, 0, SEEK_END) == -1), errcodeFromErrno(), -1);
+    sceIoLseek(fd, cur, SEEK_SET);
     return (PHYSFS_sint64)len;
 } /* __PHYSFS_platformFileLength */
 
@@ -478,7 +495,7 @@ int __PHYSFS_platformEOF(void *opaque)
 
 int __PHYSFS_platformFlush(void *opaque)
 {
-    BAIL_IF(sceIoSync("cache0:", 0) < -1, errcodeFromErrno(), 0);
+    BAIL_IF(sceIoSync("ms0:", 0) < -1, errcodeFromErrno(), 0);
     return(1);
 } /* __PHYSFS_platformFlush */
 
@@ -486,14 +503,14 @@ int __PHYSFS_platformFlush(void *opaque)
 void __PHYSFS_platformClose(void *opaque)
 {
     int fd = *((int *) opaque);
-    BAIL_IF(close(fd) == -1, errcodeFromErrno(), 0);
+    BAIL_IF(sceIoClose(fd) == -1, errcodeFromErrno(), 0);
     allocator.Free(opaque);
 } /* __PHYSFS_platformClose */
 
 
 int __PHYSFS_platformDelete(const char *path)
 {
-    BAIL_IF(remove(path) == -1, errcodeFromErrno(), 0);
+    BAIL_IF(sceIoRemove(path) == -1, errcodeFromErrno(), 0);
     return(1);
 } /* __PHYSFS_platformDelete */
 
